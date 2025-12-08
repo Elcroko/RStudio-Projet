1d6e71d334a16835624818cf3cbcf5d5a721325e
AlexandreLlv <alex95.lalevee@gmail.com>
2025-12-09 00:25:41 +0100

    Add files via upload
    travail final, moyennes de boxplot et test ANOVA simple par tailles de departements

--- Etape3.R
+++ Etape3.R
@@ -24,3 +24,45 @@ moyennes_par_taille <- data %>%
 
 # 2. Affichage du tableau de résultats
 print(moyennes_par_taille, n = Inf, width = Inf)
+
+
+
+
+
+
+
+
+# 1. Liste des colonnes cibles (noms standards R avec points à la place des espaces)
+vars_crimes <- c("Homicides", 
+                 "Violences.physiques.hors.cadre.familial", 
+                 "Vols.avec.armes", 
+                 "Vols.de.véhicule", 
+                 "Cambriolages.de.logement", 
+                 "Trafic.de.stupéfiants")
+
+# 2. Ordonner la catégorie taille (pour que les boxplots soient dans l'ordre logique)
+data$Taille_Departement <- factor(data$Taille_Departement, 
+                                  levels = c("Petit (<300k)", "Moyen", "Grand (>800k)"))
+
+# 3. Configuration de la fenêtre graphique (2 lignes x 3 colonnes)
+par(mfrow = c(2, 3), mar = c(3, 4, 2, 1))
+
+# 4. Boucle : Création Boxplot + Calcul ANOVA pour chaque crime
+for (variable in vars_crimes) {
+  
+  # A. Création du Boxplot
+  boxplot(data[[variable]] ~ data$Taille_Departement,
+          main = variable,
+          col = c("lightblue", "orange", "lightgreen"),
+          las = 1,        # Etiquettes verticales à l'endroit
+          cex.main = 0.9) # Taille du titre un peu réduite
+  
+  # B. Calcul et affichage de l'ANOVA dans la console
+  cat("\n===========================================\n")
+  cat("TEST ANOVA POUR :", variable, "\n")
+  res_anova <- aov(data[[variable]] ~ data$Taille_Departement)
+  print(summary(res_anova))
+}
+
+# Rétablir la configuration graphique par défaut
+par(mfrow = c(1, 1))
