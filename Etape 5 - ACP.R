d3cdbd5d22ebabf829bd747d0dfabab55c70bc12
164Imran <imranamazon164@gmail.com>
2025-12-15 11:14:42 +0100

    Refactor PCA analysis and visualization code
    Removed commented-out code and redundant sections while keeping the PCA analysis and visualization intact.

--- Etape 5 - ACP.R
+++ Etape 5 - ACP.R
@@ -29,25 +29,20 @@ res.pca <- PCA(df_acp, scale.unit = TRUE, graph = FALSE)
 # --- GRAPHIQUES ---
 
 # 1. Eboulis des valeurs propres
-png("eboulis.png", width=800, height=600)
 fviz_eig(res.pca, addlabels = TRUE, ylim = c(0, 70), 
          main = "Eboulis des valeurs propres")
-dev.off()
 
 # 2. Cercle des corrélations
-png("cercle.png", width=800, height=800)
 fviz_pca_var(res.pca, col.var = "cos2",
              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
              repel = TRUE,
              title = "Cercle des Corrélations (Type de délinquance)")
-dev.off()
 
 # 3. Carte des individus
-png("carte.png", width=1000, height=1000)
 fviz_pca_ind(res.pca, geom = c("point", "text"),
              col.ind = "cos2", 
              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
              repel = TRUE, # Evite le chevauchement (peut prendre qques secondes)
              labelsize = 3,
              title = "Carte des Départements")
-dev.off()
+
