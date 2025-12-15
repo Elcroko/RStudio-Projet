f2c32b6547acc9253fb1d036017ebf02916bff49
Elcrocko <churchwill78@gmail.com>
2025-12-15 11:48:12 +0100

    update Etape 2

--- Etape 2.R
+++ Etape 2.R
@@ -1,43 +1,76 @@
-# -------------------------------
-# 1) Identification variables
-# -------------------------------
-# Variables qualitatives : Code_departement, Code_region, Taille_Departement
+############################################
+# Nettoyage de l'environnement de travail
+############################################
+
+# Suppression de tous les objets présents dans l'espace de travail
+rm(list = ls())
+
+# Libération de la mémoire
+gc()
 
 
-# -------------------------------
-# 2) Fréquence des régions
-# -------------------------------
+############################################
+# Importation des données
+############################################
 
-tab <- table(df$Code_region)
+# Lecture du fichier CSV contenant les données du projet
+# stringsAsFactors = FALSE permet d'éviter la conversion automatique en facteurs
+data_proj <- read.csv("donnees_finales_projet_2024.csv", stringsAsFactors = FALSE)
 
+
+############################################
+# Analyse de la répartition des régions
+############################################
+
+# Création d'une table de fréquence des régions
+# Code_region correspond au code INSEE des régions
+tab <- table(data_proj$Code_region)
+
+# Représentation graphique : diagramme en barres
 barplot(
   tab,
   main = "Fréquence des régions",
-  xlab = "Code INSEE des régions (effectif)",
+  xlab = "Code INSEE des régions",
   ylab = "Effectif",
-  names.arg = names(tab),
-  las = 2
+  las = 2            # Orientation verticale des labels pour une meilleure lisibilité
 )
 
 
-# -------------------------------
-# 3) Répartition des départements par taille
-# -------------------------------
+############################################
+# Réinitialisation de l'environnement
+############################################
 
-# 1. Tableau des effectifs
-tab_taille <- table(df$Taille_Departement)
+rm(list = ls())
+gc()
 
-# 2. Afficher les effectifs
+
+############################################
+# Importation des données (à nouveau)
+############################################
+
+data_proj <- read.csv("donnees_finales_projet_2024.csv", stringsAsFactors = FALSE)
+
+
+############################################
+# Analyse de la taille des départements
+############################################
+
+# Table de fréquence des catégories de taille des départements
+tab_taille <- table(data_proj$Taille_Departement)
+
+# Affichage de la table de fréquence
 print(tab_taille)
 
-# 3. Pourcentages
+# Calcul des pourcentages par catégorie
 pourcentages <- prop.table(tab_taille) * 100
+
+# Affichage des pourcentages arrondis à 1 décimale
 print(round(pourcentages, 1))
 
 
-# -------------------------------
-# Barplot de la taille des départements
-# -------------------------------
+############################################
+# Représentation graphique : diagramme en barres
+############################################
 
 barplot(
   tab_taille,
@@ -50,15 +83,22 @@ barplot(
 )
 
 
-# -------------------------------
-# Diagramme circulaire (Pie chart)
-# -------------------------------
+############################################
+# Représentation graphique : diagramme circulaire
+############################################
 
-labels_pie <- paste(names(tab_taille), "\n", round(pourcentages, 1), "%")
+# Création des étiquettes avec les pourcentages
+labels_pie <- paste(
+  names(tab_taille),
+  "\n",
+  round(pourcentages, 1),
+  "%"
+)
 
+# Diagramme circulaire
 pie(
   tab_taille,
   labels = labels_pie,
   main = "Répartition par Taille de Département",
-  col = c("#66c2a5", "#fc8d62", "#8da0cb")
+  col = c("#66c2a5", "#fc8d62", "#8da0cb") # Couleurs distinctes
 )
